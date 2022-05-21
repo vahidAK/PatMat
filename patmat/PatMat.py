defbb4c9fb532ce96ff444f3a8ac0ac35f1fc6d2
Vahid Akbari <vakbari@bcgsc.ca>
2022-05-20 20:41:44 -0700

    Add existing file

--- patmat/PatMat.py
+++ patmat/PatMat.py
@@ -1,3 +1,29 @@
+#! /usr/bin/env python3
+# coding=utf-8
+
+# Copyright (C) 2020  Vahid Akbari
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+PatMat: Parent-of-origin resolved chromosome-scale haplotyping
+"""
+
+__author__ = "Vahid Akbari"
+__email__ = "vakbari@bcgsc.ca"
+__copyright__ = "Copyright (C) 2022, " + __author__
+__license__ = "GPLv3"
+
 import os
 import gzip
 import bz2
